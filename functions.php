1eabd6081187b22551297efb88da405b466ac733
devthigas502 <thiagodev502@gmail.com>
2024-04-23 13:43:49 -0300

    configurações iniciais api

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php 
+
+remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+
+function change_route_api(){
+    return 'json';
+}
+
+add_filter('rest_url_prefix', 'change_route_api');
+
+
+?>
